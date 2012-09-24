7774e597fb2beb8949b1d76b79bed9b1130d4b21
Maciej Kucharek <mkucharek@ptc.com>
2012-09-24 17:20:47 +0200

    Completed first task of WhitePages exercise. Implemented & tested the addPerson method with Fault functionality.

--- WhitePagesClient/test/ws/whitepages/test/WhitePagesTest.java
+++ WhitePagesClient/test/ws/whitepages/test/WhitePagesTest.java
@@ -5,6 +5,9 @@
 package ws.whitepages.test;
 
 import org.junit.Test;
+import ws.whitepages.AddPersonFault;
+import ws.whitepages.PersonType;
+import ws.whitepages.WhitePagesWSDLPortType;
 import static org.junit.Assert.*;
 
 /**
@@ -13,16 +16,77 @@ import static org.junit.Assert.*;
  */
 public class WhitePagesTest {
 
+    WhitePagesWSDLPortType port = (new ws.whitepages.WhitePagesWSDLService()).getWhitePagesWSDLPort();
+
     @Test
-    public void testAddPerson() {
-        ws.whitepages.WhitePagesWSDLService service = new ws.whitepages.WhitePagesWSDLService();
-        ws.whitepages.WhitePagesWSDLPortType port = service.getWhitePagesWSDLPort();
-        // TODO initialize WS operation arguments here
+    public void testAddPersonNull() {
+        testPerson(null, port);
+
+    }
+
+    @Test
+    public void testAddPersonValid() {
         ws.whitepages.PersonType person = new ws.whitepages.PersonType();
-        // TODO process result here
-        java.lang.String result = port.addPerson(person);
+        person.setFirstName("Jimmy");
+        person.setLastName("Kimmel");
+        person.setPhone("123");
 
-        assertEquals("done", result);
+        testPerson(person, port);
+    }
 
+    @Test
+    public void testAddPersonNoPhone() {
+        ws.whitepages.PersonType person = new ws.whitepages.PersonType();
+        person.setFirstName("Jimmy");
+        person.setLastName("Kimmel");
+
+        testPerson(person, port);
+    }
+
+    @Test
+    public void testAddPersonExisting() {
+        for (int i = 0; i < 2; ++i) {
+            ws.whitepages.PersonType person = new ws.whitepages.PersonType();
+            person.setFirstName("Tony");
+            person.setLastName("Hawk");
+            person.setPhone("1233");
+
+            testPerson(person, port);
+        }
+    }
+
+    private void testPerson(PersonType person, WhitePagesWSDLPortType port) {
+        try {
+            java.lang.String result = port.addPerson(person);
+            assertEquals("done", result);
+
+        } catch (AddPersonFault e) {
+            assertEquals("error", e.getMessage());
+
+            if (person == null) {
+                assertEquals("Person cannot be null", e.getFaultInfo().getErrorMessage());
+                assertNull(e.getFaultInfo().getPerson());
+
+            } else {
+
+                assertEquals(person.getFirstName(), e.getFaultInfo().getPerson().getFirstName());
+                assertEquals(person.getLastName(), e.getFaultInfo().getPerson().getLastName());
+
+                if (person.getPhone() == null) {
+                    assertEquals("Phone cannot be null", e.getFaultInfo().getErrorMessage());
+
+                } else {
+                    assertEquals("Person already exists", e.getFaultInfo().getErrorMessage());
+
+                    assertEquals(person.getPhone(), e.getFaultInfo().getPerson().getPhone());
+
+                    if (e.getFaultInfo().getPerson().getAddress() != null) {
+                        assertEquals(person.getAddress().getCity(), e.getFaultInfo().getPerson().getAddress().getCity());
+                        assertEquals(person.getAddress().getStreet(), e.getFaultInfo().getPerson().getAddress().getStreet());
+                        assertEquals(person.getAddress().getPostcode(), e.getFaultInfo().getPerson().getAddress().getPostcode());
+                    }
+                }
+            }
+        }
     }
 }
--- WhitePagesService/src/java/ws/whitepages/WhitePagesWebServiceFromWSDL.java
+++ WhitePagesService/src/java/ws/whitepages/WhitePagesWebServiceFromWSDL.java
@@ -5,6 +5,8 @@
 
 package ws.whitepages;
 
+import java.util.HashMap;
+import java.util.Map;
 import javax.jws.WebService;
 
 /**
@@ -14,8 +16,37 @@ import javax.jws.WebService;
 @WebService(serviceName = "WhitePagesWSDLService", portName = "WhitePagesWSDLPort", endpointInterface = "ws.whitepages.WhitePagesWSDLPortType", targetNamespace = "http://whitepages.ws", wsdlLocation = "WEB-INF/wsdl/WhitePagesWebServiceFromWSDL/WhitePagesWSDL.wsdl")
 public class WhitePagesWebServiceFromWSDL {
 
-    public java.lang.String addPerson(ws.whitepages.PersonType person) {
-        return "done";
+    private Map<String, PersonType> personMap = new HashMap<String, PersonType>();
+
+    public java.lang.String addPerson(ws.whitepages.PersonType person) throws AddPersonFault {
+        if (person == null) {
+            AddPersonFaultType faultInfo = new AddPersonFaultType();
+            faultInfo.setErrorMessage("Person cannot be null");
+
+            throw new AddPersonFault("error", faultInfo);
+        }
+
+        if (person.getPhone() == null) {
+            AddPersonFaultType faultInfo = new AddPersonFaultType();
+            faultInfo.setErrorMessage("Phone cannot be null");
+            faultInfo.setPerson(person);
+
+            throw new AddPersonFault("error", faultInfo);
+        }
+
+        String key = person.getFirstName() + " " + person.getLastName();
+        if (personMap.containsKey(key)) {
+
+            AddPersonFaultType faultInfo = new AddPersonFaultType();
+            faultInfo.setPerson(person);
+            faultInfo.setErrorMessage("Person already exists");
+
+            throw new AddPersonFault("error", faultInfo);
+
+        } else {
+            personMap.put(key, person);
+            return "done";
+        }
     }
 
 }
