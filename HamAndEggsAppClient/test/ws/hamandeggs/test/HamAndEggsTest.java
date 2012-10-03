8344a150f00584793ab59fbf265e3893fd4dd4b1
Maciej Kucharek <mkucharek@ptc.com>
2012-10-03 20:49:32 +0200

    Finished BPEL exercise 5.1

---
+++ HamAndEggsAppClient/test/ws/hamandeggs/test/HamAndEggsTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ws.hamandeggs.test;
+
+import java.util.Random;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kucharekm
+ */
+public class HamAndEggsTest {
+
+    private final int EGG_LIMIT = 10;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+     @Test
+     public void hello() {
+
+         try {
+             ws.hamandeggs.HamAndEggsService service = new ws.hamandeggs.HamAndEggsService();
+             ws.hamandeggs.HamAndEggsPortType port = service.getHamAndEggsPort();
+
+             // TODO initialize WS operation arguments here
+             Random rand = new Random();
+             int input = rand.nextInt(EGG_LIMIT);
+
+             // TODO process result here
+             boolean result = port.hamAndEggsOperation(input);
+
+             assertEquals(true, result);
+         } catch (Exception ex) {
+             fail();
+         }
+
+     }
+
+}
